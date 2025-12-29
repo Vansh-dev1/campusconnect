7a3df4b33278196d6c3568c54230fe5286f6c98e
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-12-29 20:53:00 +0530

    made a clean structure in project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-console.log("Hello from campusconnect!!")
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.static('public'));
+
+app.get("/info", (req, res) => {
+    res.json({
+        "message": "Hello Form CampusConnect"
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
